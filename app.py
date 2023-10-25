2b02d52993bec8348c87306ffd988988bda4243a
ovonsovych <ovonsovych@amazinum.com>
2023-10-25 14:19:43 +0300

    feat: add working segmentation on coco

--- app.py
+++ app.py
@@ -5,20 +5,28 @@ from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from ultralytics.utils.ops import scale_image
-from collections import Counter
-
-from io import BytesIO
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, Request, File, UploadFile
-from fastapi.responses import JSONResponse
-from starlette.responses import FileResponse 
+from fastapi import FastAPI, UploadFile
+from starlette.responses import Response, FileResponse 
+
+from VehicleSegmentor import VehicleSegmentor
+
+vehicle_segmentor = VehicleSegmentor()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-model = YOLO('static/models/yolov8n-seg.pt')
+
+def process_request_image(file):
+    np_image = np.frombuffer(file, np.uint8)
+    image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+    return image
+
+def process_response_image(image):
+    _, encoded_image = cv2.imencode('.PNG', image)
+    encoded_image = encoded_image.tobytes()
+    return encoded_image
 
 @app.get("/")
 async def read_index():
@@ -26,8 +34,13 @@ async def read_index():
 
 @app.post("/vehicle_segment")
 async def image_detect(file: UploadFile):
-    print(file.filename)
-    return FileResponse(file.filename, media_type="image/png")
+    file = await file.read()
+    image = process_request_image(file)
+
+    result = vehicle_segmentor.predict(image)
+
+    encoded_image = process_response_image(result)
+    return Response(encoded_image, media_type="image/png")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
